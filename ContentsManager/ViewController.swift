9c8558fe2509e0dd7b2fe314dfac0eb6c7ca7e41
Yui-00 <19rd152@ms.dendai.ac.jp>
2021-05-07 15:34:01 +0900

    Initial Commit

---
+++ ContentsManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContentsManager
+//
+//  Created by 日野原唯人 on 2021/05/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
